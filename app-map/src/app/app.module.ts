9e70fecad9f6e5875f065fd8f7dc7d94dbd56024
ellabahamady <nurlaela.bahamady@gmail.com>
2024-05-31 21:18:40 +0700

    feat: add sidebar

--- app-map/src/app/app.module.ts
+++ app-map/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { HomeComponent } from './feature/home/home.component';
+import { AboutComponent } from './feature/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
